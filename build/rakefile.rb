a1cc88e7cae6ce1a6309ccfcd29da6d8b94c8192
Hernan Garcia <hernan@dynamicprogrammer.com>
2009-12-30 00:33:33 -0500

    REbuild always

--- build/rakefile.rb
+++ build/rakefile.rb
@@ -31,7 +31,7 @@ end
 
 desc "Build solution"
 task :compile => [:prepare] do
-  sh "#{DOT_NET_PATH}msbuild.exe /p:Configuration=#{CONFIG} #{SOLUTION}"
+  sh "#{DOT_NET_PATH}msbuild.exe /p:Configuration=#{CONFIG} #{SOLUTION} /t:rebuild"
 end
 
 desc "Move dll's to package"
